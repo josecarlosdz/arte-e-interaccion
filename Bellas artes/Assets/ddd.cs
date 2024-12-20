9da16609b3dd51c58ed63f8aac0fbd8c0b0eacf2
josecarlosdz <josecarlosdiezagudo@gmail.com>
2024-12-20 12:35:58 +0100

    ñljsdfg

--- Bellas artes/Assets/ddd.cs
+++ Bellas artes/Assets/ddd.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ddd : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is haosdflasdf before the first frame update
     void Start()
     {
         
